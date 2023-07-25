07580daed501ce34ec9ab20b1ae65d58b696a060
王全 <wangq1294@fingard.com>
2023-07-25 18:41:00 +0800

    725更新

--- concurrent/src/main/java/TestReentrantLock.java
+++ concurrent/src/main/java/TestReentrantLock.java
@@ -5,10 +5,32 @@ import java.util.concurrent.locks.ReentrantLock;
  * @date : 2023/7/6 8:41
  */
 public class TestReentrantLock {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
         ReentrantLock lock = new ReentrantLock();
-        lock.lock();
-        lock.unlock();
+        Thread t1 = new Thread(()->{
+            try {
+                lock.lock();
+                System.out.println("t1获取到了锁");
+                Thread.sleep(10000000L);
+                lock.unlock();
+                System.out.println("t1释放了锁");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        Thread t2 = new Thread(()->{
+            System.out.println("t2尝试获取锁");
+            lock.lock();
+            System.out.println("t2获取到了锁");
+            lock.unlock();
+            System.out.println("t2释放了锁");
+        });
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("结束");
     }
 }
